42ea40be96bc439888bfbe0fc089ebd4cfd3a22d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-02-15 17:39:07 +0100

    add show view

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-	erb :index, layout: :main
+	erb :index, 	layout: :main
+end
+
+get "/:fish_name" do
+	@fish = params[:fish_name]
+	erb :show, 		layout: :main
 end
